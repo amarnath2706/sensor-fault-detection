5304a3d56be473b7217742ed201760f042ca968d
Amar <amarnath.arjsd@gmail.com>
2022-11-11 17:35:52 +0530

    changes made in main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 
 if __name__ == '__main__':
     mongodb_client = MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
+    print("Collection Name : " ,mongodb_client.database.list_collection_names())
