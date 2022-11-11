9d4c834abb2d8748e7fa4e2bb90d4d613777b287
Amar <amarnath.arjsd@gmail.com>
2022-11-11 17:31:35 +0530

    Changes made in mongodbconnection.py and database.py

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -8,7 +8,7 @@ class MongoDBClient:
     def __init__(self,database_name = DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodbmongodb+srv://root:Jasvee20@cluster0.ubsyogg.mongodb.net/?retryWrites=true&w=majority"
+                mongo_db_url = "mongodb+srv://root:Jasvee20@cluster0.ubsyogg.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
